eadf74b1489b7f1435a97586ad35f366b0a384b1
MarcioMattos98 <marcio98.mattos61@gmail.com>
2025-06-24 10:30:52 -0300

    feat: Implementa serviço de configuração com ConfigServiceApplication e configurações iniciais

--- config-service/src/main/java/com/luce/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/luce/configservice/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.luce.configservice;
+package main.java.com.luce.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
