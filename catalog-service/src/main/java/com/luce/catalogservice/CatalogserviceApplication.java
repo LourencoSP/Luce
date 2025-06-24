8aa7702dea84d1c3dcefdb7dd8fde3fd4cba03dc
MarcioMattos98 <marcio98.mattos61@gmail.com>
2025-06-24 11:03:25 -0300

    Refactor microservices configuration and structure
    - Updated application.yml files for review-service, user-service, and video-service to remove datasource configurations and include Spring Cloud Config server integration.
    - Modified Dockerfiles for user-service and video-service to improve ENTRYPOINT formatting.
    - Simplified pom.xml files for user-service and video-service by removing unnecessary dependencies and adding Spring Cloud dependency management.
    - Created new catalog-service with basic structure and configuration.
    - Added config-service with Spring Cloud Config Server setup and properties.
    - Implemented discovery-service with Eureka server configuration.
    - Established gateway-service with basic Spring Boot setup.
    - Added auth-service, catalog-service, discovery-service, gateway-service, review-service, user-service, and video-service configuration files in the config-repo for centralized configuration management.
    - Created main application classes for all services to bootstrap the Spring applications.

---
+++ catalog-service/src/main/java/com/luce/catalogservice/CatalogserviceApplication.java
@@ -0,0 +1,11 @@
+package com.luce.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogserviceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CatalogserviceApplication.class, args);
+  }
+}
--- config-service/src/main/java/com/luce/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/luce/configservice/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.luce.configservice;
+package com.luce.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServiceApplication.class, args);
+  }
 }
